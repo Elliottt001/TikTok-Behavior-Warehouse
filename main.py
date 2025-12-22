2331d9db3807183fe7e7ed8b28f2e74c10b1a28e
Elliottt001 <rzzhangzju@gmail.com>
2025-12-22 23:09:53 +0800

    多线程生成数据

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ import pathlib
 
 def run_pipeline():
     # 生成数据
-    data_path = pathlib.Path('../data')
-    user_header_list = ['user_id', 'nickname', 'age', 'ip', 'fans_num', 'likes_num', 'phone_type', 'register_date']
-    users_csv_path = data_path / "users.csv"
-    DataGenerator.csv_gen(users_csv_path, 5000000, DataGenerator.get_user_row, user_header_list)
-
-    video_header_list = ['user_id', 'video_id', 'video_title', 'tags', 'duration', 'upload_time']
-    video_csv_path = data_path / "video.csv"
-    DataGenerator.csv_gen(video_csv_path, 5000000, DataGenerator.get_video_row, video_header_list)
-
-    logs_json_path = data_path / "user_behavior_logs.json"
-    DataGenerator.jsonl_gen(logs_json_path, 30000000)
+    user_header_list = ['user_id', 'nickname', 'age', 'ip', 'fans_num',
+                        'likes_num', 'phone_type', 'register_date']
+    video_header_list = ['user_id', 'video_id', 'video_title', 'tags',
+                         'duration', 'upload_time']
+    
+    DataGenerator.parallel_generate(total_csv_record_count=5000000,
+                                    total_jsonl_record_count=30000000,
+                                    user_csv_headers=user_header_list,
+                                    video_csv_headers=video_header_list)
 
     # ODS and DWD 层
     db_path = '../data/tiktok_dw.db'
